dedbd17937d93b14500f0f5be43b0eacc8159aa4
Petter Skogh <pesk0026@ad.umu.se>
2019-07-29 21:30:35 +0200

    Added id to InventoryItem

--- src/com/bmengine/inventorysystem/InventoryItem.java
+++ src/com/bmengine/inventorysystem/InventoryItem.java
@@ -10,9 +10,11 @@ import java.util.LinkedList;
 
 public class InventoryItem extends GameObject {
 
+    private String id;
+
     public InventoryItem(Position position, String id, Handler handler, ObjectBounds objectBounds) {
         super(position, id, handler, objectBounds);
-
+        this.id = id;
 
     }
 
