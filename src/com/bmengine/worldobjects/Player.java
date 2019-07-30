d8b2810d7d7ca70b63559148c1268c4c47a22470
Jim Skog Pirinen <jim.skogpirinen@gmail.com>
2019-07-30 15:28:43 +0200

    A lot of resources

--- src/com/bmengine/worldobjects/Player.java
+++ src/com/bmengine/worldobjects/Player.java
@@ -16,10 +16,7 @@ public class Player extends WorldObject {
     private Position walkGoal = new Position(0,0);
 
     public Player(Position position, Handler handler, Texture texture) {
-        super(position, "player", handler, new ObjectBounds(64,64),
-                new Animation(5, texture.player[16],texture.player[17],texture.player[18],texture.player[19],texture.player[20],texture.player[21],texture.player[22]),
-                new Animation(5, texture.player[8],texture.player[9],texture.player[10],texture.player[11],texture.player[12],texture.player[13],texture.player[14],texture.player[15]),
-                new Animation(5,texture.player[0],texture.player[1],texture.player[2],texture.player[3],texture.player[4],texture.player[5],texture.player[6],texture.player[7]));
+        super(position, "player", handler, new ObjectBounds(64,64));
     }
 
     public void tick(LinkedList<GameObject> objects){
@@ -39,10 +36,11 @@ public class Player extends WorldObject {
         double direction = Math.atan2(deltaY,deltaX);
 
         setVelDir(new Position(Math.cos(direction),  Math.sin(direction)));
+
     }
 
     private void goalReached() {
-        if (Math.abs(position.getXDistance(walkGoal.getX())) < 1 || Math.abs(position.getYDistance(walkGoal.getY())) < 1) {
+        if (Math.abs(position.getXDistance(walkGoal.getX())) < 20 && Math.abs(position.getYDistance(walkGoal.getY())) < 20) {
             setVelDir(new Position(0,0));
         }
 
